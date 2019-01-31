67b7942ca1bdd1debfcbd468e9faf25843bfa075
Taoey <741494582@qq.com>
2019-01-31 18:00:15 +0800

    update

--- src/main/java/com/tao/App.java
+++ src/main/java/com/tao/App.java
@@ -1,10 +1,8 @@
 package com.tao;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.tao.dao")
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
